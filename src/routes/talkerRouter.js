eea7ea2d272e55f82e7262dd2237be6da943f2b2
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-03-14 19:28:20 -0300

    Requisite 2 Complete (The endpoint GET /talker/:id was created)

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -11,4 +11,17 @@ router.get('/', async (_req, res) => {
   return res.status(200).json(data);
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const data = await readTalkersAll();
+  const talkerPerson = data.find(({ id: findId }) => findId === Number(id));
+
+  if (!talkerPerson) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return res.status(200).json(talkerPerson);
+});
+
 module.exports = router;
